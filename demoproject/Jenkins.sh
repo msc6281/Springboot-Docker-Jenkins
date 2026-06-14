b24c92e46286993ad6acdf94578b40fa45182468
Simhachalam <93778241+msc6281@users.noreply.github.com>
2026-06-14 11:19:57 +0530

    Update Jenkins.sh

--- demoproject/Jenkins.sh
+++ demoproject/Jenkins.sh
@@ -1,11 +1,8 @@
 pipeline {
-
   agent any
-
   tools {
     maven 'Maven'
   }
-
   stages {
     stage('Checkout') {
       steps {
@@ -13,37 +10,42 @@ pipeline {
             url: 'https://github.com/msc6281/Springboot-Docker-Jenkins'
       }
     }
-
     stage('Build') {
       steps {
-        dir('demoproject') {          
+        dir('demoproject') {
           sh 'mvn clean package -DskipTests'
         }
       }
     }
-
     stage('Test') {
       steps {
-        dir('demoproject') {          
+        dir('demoproject') {
           sh 'mvn test'
         }
       }
     }
-
     stage('Docker Build') {
       steps {
-        dir('demoproject') {          
+        dir('demoproject') {
           sh 'docker build -t demoproject-ci-cd:latest .'
         }
       }
     }
-    post{
-       success{
-         echo 'Pipeline successfully completed'
-       }
-       failure{
-         echo 'Pipeline failed'
-       }
+    stage('Docker Run') {         
+      steps {
+        sh 'docker stop springapp || true'
+        sh 'docker rm springapp || true'
+        sh 'docker run -d -p 9090:8080 --name springapp demoproject-ci-cd:latest'
+      }
+    }
+  }
+
+  post {
+    success {
+      echo 'Pipeline successfully completed ✅'
+    }
+    failure {
+      echo 'Pipeline failed'
     }
   }
 }
